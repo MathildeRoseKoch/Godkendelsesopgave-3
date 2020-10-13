38424fd32fabc1eb39c8ada37891c5b0fed3426d
Mathilde Koch <mathildekoch@Mathildes-MacBook-Pro.local>
2020-10-13 11:13:27 +0200

    second commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 # Godkendelsesopgave-3
+
+const express = require("express");
+const { restart } = require("nodemon");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log('Hello world ${PORT}'));
+
+app.get ("/", function (req,res){
+    res.send("Hello World")
+});
